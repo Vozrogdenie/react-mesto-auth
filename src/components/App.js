cd70bb4f1bcb4b28cb1e195713b320fee32bb460
Vozrogdenie <93585141+Vozrogdenie@users.noreply.github.com>
2022-10-13 00:27:47 +0300

    brouserRouter

--- src/components/App.js
+++ src/components/App.js
@@ -18,43 +18,43 @@ import Login from './Login';
 import Register from './Register';
 
 function App() {
-  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen]  = useState(false);
+  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false);
   const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = useState(false);
   const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = useState(false);
   const [isYouSurePopupOpen, setIsYouSurePopupOpen] = useState(false);
   const [selectedCard, setSelectedCard] = useState(null);
-  const [currentUser, setCurrentUser] = useState({name:'', about:'', avatar:''})
+  const [currentUser, setCurrentUser] = useState({ name: '', about: '', avatar: '' })
   const [cards, setCards] = useState([])
   const [loggedIn, setLoggedIn] = useState(false)
-  const [userData, setUserData] = useState({password:'', email:''})
+  const [userData, setUserData] = useState({ password: '', email: '' })
 
   const history = useHistory();
 
-  React.useEffect(() =>{
+  React.useEffect(() => {
     tokenCheck()
   }, [])
 
-  function handleLogin (password, email)  {
+  function handleLogin(password, email) {
     return api.authorize(password, email)
-    .then((data) => {
-      if (!data.jwt) throw Error ('Not jwt');
+      .then((data) => {
+        if (!data.jwt) throw Error('Not jwt');
 
-      localStorage.setItem('jwt', data.jwt);
-      setLoggedIn(true);
-      setUserData({
-        password: data.password,
-        email: data.email
-        
-    });
-      history.push('/')
-    })
+        localStorage.setItem('jwt', data.jwt);
+        setLoggedIn(true);
+        setUserData({
+          password: data.password,
+          email: data.email
+
+        });
+        history.push('/')
+      })
   }
 
-  function handleRegister (password, email) {
+  function handleRegister(password, email) {
     return api.register(password, email)
-    .then(() => {
-      history.push('/signin')
-    })
+      .then(() => {
+        history.push('/signin')
+      })
   }
   // const handleLogout = () => {
   //   localStorage.removeItem('jwt');
@@ -62,18 +62,18 @@ function App() {
   //   history.push('/login');
   // }
 
-  function tokenCheck  () {
-  const jwt = localStorage.getItem('jwt')
-  if (!jwt) return;
-  api.getContent(jwt).then((data) => {
-    setLoggedIn(true);
-    setUserData({
-      password: data.user.password,
-      email: data.user.email
+  function tokenCheck() {
+    const jwt = localStorage.getItem('jwt')
+    if (!jwt) return;
+    api.getContent(jwt).then((data) => {
+      setLoggedIn(true);
+      setUserData({
+        password: data.user.password,
+        email: data.user.email
 
+      })
+      history.push('/')
     })
-    history.push('/')
-  })
 
   }
   function handleCardClick(card) {
@@ -98,32 +98,32 @@ function App() {
     setIsEditProfilePopupOpen(false)
     setIsAddPlacePopupOpen(false)
   };
-  
+
   React.useEffect(() => {
     api.getApiCards()
-        .then((res) => {
-            setCards(res)
-        }).catch((err)=>{
-            console.log(err)
-        });
+      .then((res) => {
+        setCards(res)
+      }).catch((err) => {
+        console.log(err)
+      });
   }, []);
 
   function handleCardLike(card) {
-      const isLiked = card.likes.some(i => i._id === currentUser._id);
-      api.changeLikeCardStatus(card._id, isLiked).then((newCard) => {
-          setCards((state) => state.map((c) => c._id === card._id ? newCard : c))
-      }).catch((err)=>{
-        console.log(err)
+    const isLiked = card.likes.some(i => i._id === currentUser._id);
+    api.changeLikeCardStatus(card._id, isLiked).then((newCard) => {
+      setCards((state) => state.map((c) => c._id === card._id ? newCard : c))
+    }).catch((err) => {
+      console.log(err)
     });;
-  } ;
+  };
 
   function handleCardDelete(card) {
     api.deleteCard(card._id)
       .then(() => {
         setCards(cards.filter(c => c._id !== card._id))
-      }).catch((err)=>{
+      }).catch((err) => {
         console.log(err)
-      });   
+      });
   };
 
   function handleUpdateAvatar(avatar) {
@@ -131,90 +131,83 @@ function App() {
       .then((res) => {
         setCurrentUser(res)
         closeAllPopups();
-      }).catch((err)=>{
+      }).catch((err) => {
         console.log(err)
-    });
+      });
   };
-  
+
   function handleUpdateUser(name, about) {
     api.setApiUsers(name, about)
-    .then(res => {
-      setCurrentUser(res);
-      closeAllPopups();
-    }).catch((err)=>{
-      console.log(err)
-  });
-};
+      .then(res => {
+        setCurrentUser(res);
+        closeAllPopups();
+      }).catch((err) => {
+        console.log(err)
+      });
+  };
 
   function handleAddPlaceSubmit(card) {
     api.createCards(card)
-    .then(newCard => {
-      setCards([newCard, ...cards]);
-      closeAllPopups(); 
-    }).catch((err) => {
-      console.log(err)
-    });
+      .then(newCard => {
+        setCards([newCard, ...cards]);
+        closeAllPopups();
+      }).catch((err) => {
+        console.log(err)
+      });
   };
 
   React.useEffect(() => {
     api.getApiUsers()
-        .then((name, about) => {
-          setCurrentUser(name, about)
-        }).catch((err)=>{
-            console.log(err)
-        })  
-}, []);
+      .then((name, about) => {
+        setCurrentUser(name, about)
+      }).catch((err) => {
+        console.log(err)
+      })
+  }, []);
 
   return (
-
     <div>
-        
-        <BrowserRouter>
-          <Switch>  
-
-            <ProtectedRoute exact path="/" loggedIn={loggedIn} >
+        <Switch>
+          <ProtectedRoute exact path="/" loggedIn={loggedIn} >
             <CurrentUserContext.Provider value={currentUser}>
-    <CurrentCardContext.Provider value={cards}>
-              <Main
-                onEditProfile={handleEditProfileClick}
-                onAddPlace={handleAddPlaceClick}
-                onEditAvatar={handleEditAvatarClick}
-                handleCardClick={handleCardClick}
-                onCardLike={handleCardLike}
-                onCardDelete={handleCardDelete}
-              />
-              <PopupEditProfile opened={isEditProfilePopupOpen} onUpdateUser={handleUpdateUser}  onClosePopup={closeAllPopups}></PopupEditProfile> 
-              <PopupAddPlace opened={isAddPlacePopupOpen} onPlace={handleAddPlaceSubmit} onClosePopup={closeAllPopups}></PopupAddPlace>
-              <PopupEditAvatar opened={isEditAvatarPopupOpen} onUpdateAvatar={handleUpdateAvatar} onClosePopup={closeAllPopups}></PopupEditAvatar>
-              <ImagePopup card={selectedCard} onClose={closeAllPopups} />
-              <PopupWithForm name="you-sure" opened={isYouSurePopupOpen} title='Вы уверены?'buttonText='Да' onClosePopup={closeAllPopups}><PopupYouSure/></PopupWithForm>
+              <CurrentCardContext.Provider value={cards}>
+                <Main
+                  onEditProfile={handleEditProfileClick}
+                  onAddPlace={handleAddPlaceClick}
+                  onEditAvatar={handleEditAvatarClick}
+                  handleCardClick={handleCardClick}
+                  onCardLike={handleCardLike}
+                  onCardDelete={handleCardDelete}
+                />
+                <PopupEditProfile opened={isEditProfilePopupOpen} onUpdateUser={handleUpdateUser} onClosePopup={closeAllPopups}></PopupEditProfile>
+                <PopupAddPlace opened={isAddPlacePopupOpen} onPlace={handleAddPlaceSubmit} onClosePopup={closeAllPopups}></PopupAddPlace>
+                <PopupEditAvatar opened={isEditAvatarPopupOpen} onUpdateAvatar={handleUpdateAvatar} onClosePopup={closeAllPopups}></PopupEditAvatar>
+                <ImagePopup card={selectedCard} onClose={closeAllPopups} />
+                <PopupWithForm name="you-sure" opened={isYouSurePopupOpen} title='Вы уверены?' buttonText='Да' onClosePopup={closeAllPopups}><PopupYouSure /></PopupWithForm>
               </CurrentCardContext.Provider>
-    </CurrentUserContext.Provider>           
-            </ProtectedRoute>
-            <Route  path="/signup">
-            <Header 
+            </CurrentUserContext.Provider>
+          </ProtectedRoute>
+          <Route path="/signup">
+            <Header
               link={'Войти'}
-              
               path='/signin'
-              />
-              <Register  onRegister={handleRegister}/>
-            </Route>     
-            <Route  path="/signin">
-            <Header 
+            />
+            <Register onRegister={handleRegister} />
+          </Route>
+          <Route path="/signin">
+            <Header
               link={'Зарегистрироваться'}
-              
               path='/signup'
-              />    
-              <Login  onLogin={handleLogin}/>
-            </Route> 
-            <Route exact path="/">
-              {loggedIn ? <Redirect to="/" /> : <Redirect to="/signin" />}
-            </Route> 
-          </Switch>
-        </BrowserRouter>
-        <Footer />
-        </div>
+            />
+            <Login onLogin={handleLogin} />
+          </Route>
+          <Route exact path="/">
+            {loggedIn ? <Redirect to="/" /> : <Redirect to="/signin" />}
+          </Route>
+        </Switch>
+      <Footer />
+    </div>
   );
 };
-  
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from '../src/components/App';
+import { BrowserRouter } from 'react-router-dom';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
-    <App />
-  
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
